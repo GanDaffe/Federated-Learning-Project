0a4f655c6affac80265850050693ce2870e1603b
Quang Duy <121656882+GanDaffe@users.noreply.github.com>
2025-02-26 11:37:10 +0700

    Update run_helper.py
    Sửa lỗi code

--- run_helper.py
+++ run_helper.py
@@ -1,9 +1,8 @@
 import flwr as fl
 import torch
-from utils import get_parameters
 from flwr.common import ndarrays_to_parameters
 from clients import FlowerClient, FedAdmImpClient, FedProxClient, ScaffoldClient
-from utils.training_utils import get_model
+from utils.training_utils import get_model, get_parameters
 from import_fed import *
 
 def run_simulation(
@@ -21,20 +20,22 @@ def run_simulation(
         entropies = None    
     ):
     net = get_model(model, dataset).to(device)  
+    # print(f"Type of net: {type(net)}")
+    # print(f"Expected model but got: {net}")
 
     def scaffold_client_fn(cid: str) -> ScaffoldClient:
         idx = int(cid)
-        return ScaffoldClient(cid, net, trainloader=trainloader[idx], valloader=valloader[idx], criterion=criterion)
+        return ScaffoldClient(cid, net, trainloader[idx], valloader[idx], criterion).to_client()
     
     def normal_client_fn(cid: str) -> FlowerClient:
         idx = int(cid)
-        return FlowerClient(cid, net, trainloader=trainloader[idx], valloader=valloader[idx], criterion=criterion)
+        return FlowerClient(cid, net,  criterion, trainloader[idx], valloader[idx]).to_client()
     
     def cluster_fed_client_fn(cid: str) -> FedAdmImpClient:
         idx = int(cid)
-        return FedAdmImpClient(cid, net, cluster_id=client_cluster_index[idx], criterion=criterion, trainloader=trainloader[idx], valloader=valloader[idx])
+        return FedAdmImpClient(cid, client_cluster_index[idx], net, criterion, trainloader[idx], valloader[idx]).to_client()
 
-    
+
     current_parameters = ndarrays_to_parameters(get_parameters(net))
     client_resources = {"num_cpus": 2, "num_gpus": 1} if device.type == "cuda" else {"num_cpus": 1, "num_gpus": 0.0}
 
@@ -73,7 +74,7 @@ def run_simulation(
         )   
     elif algo == 'fedprox': 
         fl.simulation.start_simulation(
-            client_fn = normal_client_fn-456,
+            client_fn = normal_client_fn,
             num_clients = num_clients,
             config = fl.server.ServerConfig(num_rounds=num_round),
             strategy = FedProx(num_rounds=num_round,
@@ -105,10 +106,11 @@ def run_simulation(
             client_fn = normal_client_fn,
             num_clients = num_clients,
             config = fl.server.ServerConfig(num_rounds=num_round),
-            strategy = FedImp(num_rounds=num_round,
+            strategy = FedAdp(num_rounds=num_round,
                               num_clients=num_clients,
                               iids = iids,
                               current_parameters=current_parameters,
+                              # entropies=entropies
                               # learning_rate = 0.1
                             ),
 
