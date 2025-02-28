28b8781176284ff269f6dc7a4d74c777627bb06d
Quang Duy <121656882+GanDaffe@users.noreply.github.com>
2025-02-28 16:53:45 +0700

    Update scaffold.py

--- federated_algo/scaffold.py
+++ federated_algo/scaffold.py
@@ -1,110 +1,96 @@
-from __init__ import * 
+from __init__ import *
+from flwr.server.strategy import FedAvg
 
-class SCAFFOLD(fl.server.strategy.Strategy):
+class SCAFFOLD(FedAvg):
+
     def __init__(
         self,
-        num_rounds: int,
-        num_clients: int,
-        iids: int,
-        learning_rate: float = 0.1,
+        global_model,
+        num_rounds,
+        num_clients,
+        iids,
+        learning_rate = 0.1,
+        weight_decay = 0.001,
         fraction_fit: float = 1.0,
         fraction_evaluate: float = 1.0,
         min_fit_clients: int = 2,
         min_evaluate_clients: int = 2,
         min_available_clients: int = 2,
-        weight_shapes: List[Tuple[int, ...]] = None,
-        current_parameters: Optional[Parameters] = None,
+        initial_parameters: Optional[Parameters] = None
     ) -> None:
         super().__init__()
-        self.initial_parameters = parameters_to_ndarrays(current_parameters)
 
         self.num_rounds = num_rounds
         self.num_clients = num_clients
+        self.iids = iids
+        self.global_learning_rate = learning_rate
+        self.weight_decay = weight_decay
         self.fraction_fit = fraction_fit
         self.fraction_evaluate = fraction_evaluate
         self.min_fit_clients = min_fit_clients
         self.min_evaluate_clients = min_evaluate_clients
         self.min_available_clients = min_available_clients
-        self.iids = iids
-        self.learning_rate = learning_rate
-        self.weight_shapes = list(map(lambda arr: arr.shape, self.initial_parameters)) # shapes used for checking weights and covariates
-        self.covariates = [np.zeros(shape) for shape in self.weight_shapes] # server covariates
+        self.initial_parameters = initial_parameters
 
-        # store client_covariates on server side because clients are generated on demand. Instead of each client holding its state
-        # as the Scaffold paper describes, we store the client_covariates on server side. This of course can be changed.
-        self.clients_covariates: Dict[str, NDArrays] = {} # cid -> client_covariates
 
-        self.covariates_zero = [np.zeros(shape) for shape in self.weight_shapes] # initial client covariates
-        self.result = {"round": [], "train_loss": [], "train_accuracy": [], "test_loss": [], "test_accuracy": []}
-        self.evaluate_fn = None
+        self.c_global = [torch.zeros_like(param) for param in global_model.parameters()]
+        self.current_weights = parameters_to_ndarrays(self.initial_parameters)
+        self.num_clients = num_clients
+
+        self.result = {"round": [], "test_loss": [], "test_accuracy": []}
 
     def __repr__(self) -> str:
-        return "SCAFFOLD"
+        return 'SCAFFOLD'
 
-    def _unpack_parameters(self, parameters: Parameters) -> Tuple[NDArrays, NDArrays]:
-        """Extract weights and covariates from parameters"""
-        weights_and_covariates = parameters_to_ndarrays(parameters)
-        # print_shapes(weights_and_covariates, 'weights_and_covariates')
-        self._check_shapes(weights_and_covariates)
-        weights = weights_and_covariates[:len(weights_and_covariates)//2]
-        covariates = weights_and_covariates[len(weights_and_covariates)//2:]
-        return weights, covariates
+    def num_fit_clients(self, num_available_clients: int) -> tuple[int, int]:
+        """Return the sample size and the required number of available clients."""
+        num_clients = int(num_available_clients * self.fraction_fit)
+        return max(num_clients, self.min_fit_clients), self.min_available_clients
 
-    def _pack_weights_and_covariates(
-        self,
-        weights: NDArrays,
-        server_covariates: Optional[NDArrays] = None,
-        client_covariates: Optional[NDArrays] = None,
-      ) -> Parameters:
-        """Convert weights and covariates to parameters"""
-        weights_and_covariates = (
-            weights
-          + (server_covariates if server_covariates is not None else [])
-          + (client_covariates if client_covariates is not None else [])
-        )
-        self._check_shapes(weights_and_covariates)
-        return ndarrays_to_parameters(weights_and_covariates)
-
-    def _check_shapes(self, weights_and_covariates: NDArrays) -> None:
-        """check shapes of weights and covariates"""
-        check_shapes(weights_and_covariates, self.weight_shapes)
+    def num_evaluation_clients(self, num_available_clients: int) -> tuple[int, int]:
+        """Use a fraction of available clients for evaluation."""
+        num_clients = int(num_available_clients * self.fraction_evaluate)
+        return max(num_clients, self.min_evaluate_clients), self.min_available_clients
 
     def initialize_parameters(
         self, client_manager: ClientManager
     ) -> Optional[Parameters]:
-        """Initialize global model parameters."""
-        return self._pack_weights_and_covariates(self.initial_parameters, self.covariates)
+        """Initialize global model parameters."""      
+        return self.initial_parameters
+    def evaluate(
+        self, server_round: int, parameters: Parameters
+    ) -> Optional[tuple[float, dict[str, Scalar]]]:
+        """Evaluate model parameters using an evaluation function."""
+
+        return None
 
     def configure_fit(
-        self, server_round: int, parameters: Parameters, client_manager: ClientManager
+        self, server_round, parameters, client_manager
     ) -> List[Tuple[ClientProxy, FitIns]]:
         """Configure the next round of training."""
-        # unpack parameters
-        weights, server_covariates = self._unpack_parameters(parameters)
+        # Get the standard client/config pairs from the FedAvg super-class
+        client_config_pairs = super().configure_fit(
+              server_round, parameters, client_manager
+          )
+        # Serialize c_global to be compatible with config FitIns return values
+        c_global = []
+        for param in self.c_global:
+            c_global += param.flatten().tolist()  # Flatten all params
+            
+        global_c_numpy = np.array(c_global, dtype=np.float64)
+        global_c_bytes = global_c_numpy.tobytes()
 
-        # Sample clients
-        sample_size, min_num_clients = self.num_fit_clients(
-            client_manager.num_available()
-        )
-        clients = client_manager.sample(
-            num_clients=sample_size, min_num_clients=min_num_clients
-        )
-
-        # Create custom params per client
-        fit_configurations = []
-        config = {"learning_rate": self.learning_rate,
-                  "weight_decay": 0.001,
-                  "num_worker": 2,
-                  "epochs": 1
-                  }
-        print(f"Server config: {config}")  # Debug
-        for client in clients:
-            # we need to send client_covariates specific to each client
-            client_covariates = self.clients_covariates.get(client.cid, self.covariates_zero)
-            parameters = self._pack_weights_and_covariates(weights, server_covariates, client_covariates)
-            fit_configurations.append((client, FitIns(parameters, config)))
-
-        return fit_configurations
+        # Return client/config pairs with the c_global serialized control variate
+        return [
+            (
+                client,
+                FitIns(
+                    fit_ins.parameters,
+                    {**fit_ins.config, "c_global": global_c_bytes},
+                ),
+            )
+            for client, fit_ins in client_config_pairs
+        ]
 
     def aggregate_fit(
         self,
@@ -113,75 +99,57 @@ class SCAFFOLD(fl.server.strategy.Strategy):
         failures: List[Union[Tuple[ClientProxy, FitRes], BaseException]],
     ) -> Tuple[Optional[Parameters], Dict[str, Scalar]]:
         """Aggregate fit results using weighted average."""
-        weights_results = []
-        for client_proxy, fit_res in results:
-            weights, client_covariates = self._unpack_parameters(fit_res.parameters)
+        parameters_aggregated, metrics_aggregated = super().aggregate_fit(server_round, results, failures)
+        if parameters_aggregated is None:
+            return None, {}
 
-            self.clients_covariates[client_proxy.cid] = client_covariates
+        fedavg_weights_aggregate = parameters_to_ndarrays(parameters_aggregated)
 
-            weight = 1
-            weights_results.append((weights, weight))
+        # Aggregating the updates of y_delta to current weight cf. Scaffold equation (n°5)
+        for current_weight, fed_weight in zip(self.current_weights, fedavg_weights_aggregate):
+            current_weight += fed_weight * self.global_learning_rate
 
-        # equation (5)(i) - Scaffold paper
-        # no need to use previous weights because the server learning rate is 1
-        # and so mathematically the update depends only on the updated weights from clients
-        weights_aggregated = aggregate(weights_results)
-        covariates_list = list(self.clients_covariates.values()) 
-        # equation (5)(ii) - Scaffold paper
-        # similar trick as previously - no need to use previous server covariates
-        # server_covariates = list(np.sum(list(self.clients_covariates.values()), axis=0) / self.num_clients)
-        server_covariates = [
-            np.sum([client_cov[i] for client_cov in covariates_list], axis=0) / self.num_clients
-            for i in range(len(self.weight_shapes))
-        ]
-        parameters_aggregated = self._pack_weights_and_covariates(weights_aggregated, server_covariates)
-        metrics_aggregated = {}
+        # Initalize c_delta_sum for the weight average
+        c_delta_sum = [np.zeros_like(c_global) for c_global in self.c_global]
 
-        num_examples = [fit_res.num_examples for _, fit_res in results]
+        for _, fit_res in results:
+            # Getting serialized buffer from fit metrics 
+            c_delta = np.frombuffer(fit_res.metrics["c_delta"], dtype=np.float64)
+            # Sum all c_delta in a single weight vector
+            for i in range(len(c_delta_sum)):
+                c_delta_sum[i] += np.array(c_delta[i], dtype=np.float64)
 
-        losses = [fit_res.num_examples * fit_res.metrics["loss"] for _, fit_res in results]
-        corrects = [round(fit_res.num_examples * fit_res.metrics["accuracy"]) for _, fit_res in results]
-        loss = sum(losses) / sum(num_examples)
-        accuracy = sum(corrects) / sum(num_examples)
-        print(f"train_loss: {loss} - train_acc: {accuracy}")
+        for i in range(len(self.c_global)):
+            # Aggregating the updates of c_global cf. Scaffold equation (n°5)
+            c_delta_avg = c_delta_sum[i] / self.num_clients
+            self.c_global[i] += torch.tensor(c_delta_avg)
 
-        self.result["round"].append(server_round)
-        self.result["train_loss"].append(loss)
-        self.result["train_accuracy"].append(accuracy)
+        return ndarrays_to_parameters(self.current_weights), metrics_aggregated
 
-        return parameters_aggregated, metrics_aggregated
 
     def configure_evaluate(
-        self, server_round: int, parameters: Parameters, client_manager: ClientManager
+        self,
+        server_round: int,
+        parameters: Parameters,
+        client_manager: ClientManager
     ) -> List[Tuple[ClientProxy, EvaluateIns]]:
         """Configure the next round of evaluation."""
-        # unpack parameters
-        weights, _server_covariates = self._unpack_parameters(parameters)
 
-        if self.fraction_evaluate == 0.0:
-            return []
+        sample_size, min_num_clients = self.num_evaluation_clients(client_manager.num_available())
+        clients = client_manager.sample(num_clients=sample_size, min_num_clients=min_num_clients)
 
-        # for local evaluation, we don't need server_covariates neither client_covariates
+        config = {}
+        evaluate_ins = EvaluateIns(parameters, config)
 
-        configs = {}
-        evaluate_ins = EvaluateIns(ndarrays_to_parameters(weights), configs)
-        # Sample clients
-        sample_size, min_num_clients = self.num_evaluation_clients(
-            client_manager.num_available()
-        )
-        clients = client_manager.sample(
-            num_clients=sample_size, min_num_clients=min_num_clients
-        )
-
-        # Return client/config pairs
-        return [(client, evaluate_ins) for client in clients]
+        evaluate_configs = [(client, evaluate_ins) for client in clients]
+        return evaluate_configs
 
     def aggregate_evaluate(
         self,
         server_round: int,
-        results: List[Tuple[ClientProxy, EvaluateRes]],
-        failures: List[Union[Tuple[ClientProxy, EvaluateRes], BaseException]],
-    ) -> Tuple[Optional[float], Dict[str, Scalar]]:
+        results: list[tuple[ClientProxy, EvaluateRes]],
+        failures: list[Union[tuple[ClientProxy, EvaluateRes], BaseException]],
+    ) -> tuple[Optional[float], dict[str, Scalar]]:
         """Aggregate evaluation losses using weighted average."""
         if not results:
             return None, {}
@@ -192,43 +160,19 @@ class SCAFFOLD(fl.server.strategy.Strategy):
                 for _, evaluate_res in results
             ]
         )
-        metrics_aggregated = {}
         corrects = [round(evaluate_res.num_examples * evaluate_res.metrics["accuracy"]) for _, evaluate_res in results]
         examples = [evaluate_res.num_examples for _, evaluate_res in results]
         accuracy = sum(corrects) / sum(examples)
         print(f"test_loss: {loss_aggregated} - test_acc: {accuracy}")
 
+        self.result["round"].append(server_round)
         self.result["test_loss"].append(loss_aggregated)
         self.result["test_accuracy"].append(accuracy)
 
-        os.makedirs("result", exist_ok=True)
         if server_round == self.num_rounds:
             df = pd.DataFrame(self.result)
-            df.to_csv(f"result/scaffold_numiid_{self.iids}.csv", index=False)
+            df.to_csv(f"result/scaffold_{self.iids}.csv", index=False)
+
+        metrics_aggregated = {}
 
         return loss_aggregated, metrics_aggregated
-
-    def evaluate(
-        self, server_round: int, parameters: Parameters
-    ) -> Optional[Tuple[float, Dict[str, Scalar]]]:
-        """Evaluate model parameters using an evaluation function."""
-        if self.evaluate_fn is None:
-            # No evaluation function provided
-            return None
-
-        weights, server_covariates = self._unpack_parameters(parameters)
-        eval_res = self.evaluate_fn(server_round, weights, {}) # using weights only for the evaluation
-        if eval_res is None:
-            return None
-
-        return eval_res
-
-    def num_fit_clients(self, num_available_clients: int) -> Tuple[int, int]:
-        """Return sample size and required number of clients."""
-        num_clients = int(num_available_clients * self.fraction_fit)
-        return max(num_clients, self.min_fit_clients), self.min_available_clients
-
-    def num_evaluation_clients(self, num_available_clients: int) -> Tuple[int, int]:
-        """Use a fraction of available clients for evaluation."""
-        num_clients = int(num_available_clients * self.fraction_evaluate)
-        return max(num_clients, self.min_evaluate_clients), self.min_available_clients
