7927c79fe82a168a5a405374cbc03e354be3eca8
Quang Duy <121656882+GanDaffe@users.noreply.github.com>
2025-02-28 16:53:11 +0700

    Update training_utils.py
    Thêm hai hàm hỗ trợ cho scaffold

--- utils/training_utils.py
+++ utils/training_utils.py
@@ -25,6 +25,33 @@ if torch.cuda.is_available():
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+def load_c_local(partition_id: int):
+    path = "c_local_folder/" + str(partition_id) +".txt"
+    if os.path.exists(path):
+        with open(path, 'rb') as f:
+            c_delta_bytes = f.read()
+
+        array = np.frombuffer(c_delta_bytes, dtype=np.float64)
+        return array
+    else:
+        return None
+
+# Custom function to serialize to bytes and save c_local variable inside a file
+def set_c_local(partition_id: int, c_local):
+    path = "training_process_files/c_local_folder/" + str(partition_id) +".txt"
+
+    if not os.path.exists(path):
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+    c_local_list = []
+    for param in c_local:
+        c_local_list += param.flatten().tolist()
+
+    c_local_numpy = np.array(c_local_list, dtype=np.float64)
+    c_local_bytes = c_local_numpy.tobytes()
+
+    with open(path, 'wb') as f:
+        f.write(c_local_bytes)
+
 
 def get_model(model_name, dataset_name, num_conv_block=4):
     """
