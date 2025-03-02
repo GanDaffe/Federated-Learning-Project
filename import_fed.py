c0dad5ffc2679276a87a91835a738edbb80c931d
Quang Duy <121656882+GanDaffe@users.noreply.github.com>
2025-03-02 16:46:53 +0700

    Update import_fed.py

--- import_fed.py
+++ import_fed.py
@@ -1,10 +1,23 @@
 from federated_algo.fedadp import FedAdp
-from federated_algo.fedavg import FedAvg, FedAvgM 
+from federated_algo.fedavgM import FedAvgM 
 from federated_algo.fedAdam import FedAdam
 from federated_algo.fedAdaGrad import FedAdagrad
-from federated_algo.fedAdpImp import BoxFedv2
+
 from federated_algo.dyfedimp import DyFedImp
 from federated_algo.fedimp import FedImp
-from federated_algo.fedprox import FedProx
-from federated_algo.scaffold import SCAFFOLD
 from federated_algo.fedyogi import FedYogi
+
+from federated_algo.fednova.client import FedNovaClient
+from federated_algo.fednova.strategy import FedNova
+
+from federated_algo.base.base_client import BaseClient
+from federated_algo.base.base_algo import FedAvg
+
+from federated_algo.fedadpimp.client import FedAdmImpClient
+from federated_algo.fedadpimp.strategy import BoxFedv2
+
+from federated_algo.scaffold.client import SCAFFOLD_CLIENT
+from federated_algo.scaffold.strategy import SCAFFOLD
+
+from federated_algo.fedprox.client import FedProxClient
+from federated_algo.fedprox.strategy import FedProx
