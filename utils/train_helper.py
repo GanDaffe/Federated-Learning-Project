98fa10af438a18b4bed4761205eada8f2244d2ab
Quang Duy <121656882+GanDaffe@users.noreply.github.com>
2025-03-08 17:51:53 +0700

    Update train_helper.py

--- utils/train_helper.py
+++ utils/train_helper.py
@@ -1,176 +1,265 @@
 import numpy as np
-from collections import Counter, OrderedDict
-from typing import List, Dict
+from typing import List
 import random
-import math
-import copy
-import gc
 import torch
+import pandas as pd 
+from collections import Counter
+from torch.distributions.dirichlet import Dirichlet
+from torchvision.datasets import CIFAR10, CIFAR100, EMNIST, FashionMNIST
+import torchvision.transforms as transforms
 import torch.nn as nn
-from torch.optim import SGD
-from models import CNN, LSTM, ResNet, ResidualBlock, VGG16
+from sklearn.cluster import OPTICS
+from models import CustomDataset
 from tqdm import tqdm 
+import string
+import emoji
+from utils.distance import hellinger, jensen_shannon_divergence_distance
 
-seed_value = 42
-random.seed(seed_value)
-torch.manual_seed(seed_value)
+def clean_text(tweet):
+    import re
 
-if torch.cuda.is_available():
-    torch.cuda.manual_seed_all(seed_value)
+    urlPattern        = r"((http://)[^ ]*|(https://)[^ ]*|(www\.)[^ ]*)"
+    userPattern       = '@[^\s]+'
+    hashtagPattern    = '#[^\s]+'
+    sequencePattern   = r"(.)\1\1+"
+    seqReplacePattern = r"\1\1"
 
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    tweet = tweet.lower()
 
+    tweet = re.sub(urlPattern, '', tweet)
 
-def get_model(model_name, dataset_name, model_config):
-    """
-    Only modify the num_conv_block if you want to use CustomCNN model
-    """
+    tweet = re.sub(userPattern, '', tweet)
+
+    tweet = re.sub(sequencePattern, seqReplacePattern, tweet)
+
+    tweet = re.sub(r'/', ' / ', tweet)
+
+    tweet = emoji.replace_emoji(tweet, replace='')
+
+    tweet = tweet.replace('\r', '').replace('\n', ' ').replace('\n', ' ').lower()
+    tweet = re.sub(r"(?:\@|https?\://)\S+", "", tweet)
+    tweet = re.sub(r'[^\x00-\x7f]', r'', tweet)
+    banned_list = string.punctuation + 'Ã' + '±' + 'ã' + '¼' + 'â' + '»' + '§'
+    table = str.maketrans('', '', banned_list)
+    tweet = tweet.translate(table)
+
+    tweet = " ".join(word.strip() for word in re.split('#(?!(?:hashtag)\b)[\w-]+(?=(?:\s+#[\w-]+)*\s*$)', tweet))
+    tweet = " ".join(word.strip() for word in re.split('#|_', tweet))
+
+    tweet = ' '.join([word if ('$' not in word) and ('&' not in word) else '' for word in tweet.split(' ')])
+
+    tweet = re.sub("\s\s+", " ", tweet)
+
+    return tweet.strip()
+
+def renormalize(dist: torch.tensor, labels: List[int], label: int):
+    idx = labels.index(label)
+    dist[idx] = 0
+    dist /= sum(dist)
+    dist = torch.concat((dist[:idx], dist[idx+1:]))
+    return dist
+
+def load_data(dataset: str):
+    if dataset == "cifar10":
+        train_transform = transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+
+        test_transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+
+        trainset = CIFAR10("data", train=True, download=True, transform=train_transform)
+        testset = CIFAR10("data", train=False, download=True, transform=test_transform)
+        datatype = 'image'
+
+    elif dataset == "emnist":
+        transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.5), (0.5))
+        ])
+
+        trainset = EMNIST("data", split="balanced", train=True, download=True, transform=transform)
+        testset = EMNIST("data", split="balanced", train=False, download=True, transform=transform)
+
+    elif dataset == "fmnist":
+        transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.5), (0.5))
+        ])
+
+        trainset = FashionMNIST(root='data', train=True, download=True, transform=transform)
+        testset = FashionMNIST(root='data', train=True, download=True, transform=transform)
+
+    elif dataset == 'cifar100':
+        train_transform = transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+
+        test_transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+
+        trainset = CIFAR100("data", train=True, download=True, transform=train_transform)
+        testset = CIFAR100("data", train=False, download=True, transform=test_transform)
+
+    elif dataset == 'sentimen140':
+        import gdown
+        from sklearn.model_selection import train_test_split
+        import zipfile
+        from pathlib import Path
+        from keras.preprocessing.text import Tokenizer
+        from keras.preprocessing.sequence import pad_sequences
+
+        file_path = Path("/content/dataset/training.1600000.processed.noemoticon.csv")
+
+        if file_path.exists():
+            print("Dataset is already dowloaded!")
+        else:
+            file_id = '1AN_svT4t-3U7otJjavvy-2SCXBX-NyBb'
+            output_path = "dataset.zip"
+
+            gdown.download(f"https://drive.google.com/uc?id={file_id}", output_path, quiet=False)
+
+            with zipfile.ZipFile("dataset.zip", "r") as zip_ref:
+                zip_ref.extractall("dataset")
+
+        columns = ['label', 'id', 'date', 'flag', 'user', 'text']
+        df = pd.read_csv(file_path, names=columns, encoding='latin1')
+        df = df[['label' ,'text']]
+
+        df['label'] = df['label'].replace({4: 1} )
+        df['preprocessing_text'] = df['text'].apply(clean_text)
+
+        print(df.head())
+        max_words = 2000
+        max_len = 500
+
+        tokenizer = Tokenizer(num_words=max_words)
+        tokenizer.fit_on_texts(df['text'])
+        sequences = tokenizer.texts_to_sequences(df['preprocessing_text'])
+
+        X = pad_sequences(sequences, maxlen=max_len)
+        y = df['label'].values
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        trainset = CustomDataset(X_train, y_train)
+        testset = CustomDataset(X_test, y_test)
+
+    return trainset, testset
+
+def clustering(classes, data_size, num_clients, dist, min_smp=3, xi=0.01, distance='manhattan'):
+    distrib_ = []
+    for d in dist:
+      distrib_.append(np.array([d[s] / (data_size / num_clients) if d[s] is not None else 0 for s in classes]))
+
+    if distance == 'hellinger':
+        optics = OPTICS(min_samples=min_smp,
+                        xi=xi,
+                        metric=hellinger,
+                        min_cluster_size=2)
+    elif distance == 'jensenshannon':
+        optics = OPTICS(min_samples=min_smp,
+                        xi=xi,
+                        metric=jensen_shannon_divergence_distance,
+                        min_cluster_size=2)
+    else:
+        optics = OPTICS(min_samples=min_smp,
+                        xi=xi,
+                        metric=distance,
+                        min_cluster_size=2)
+
+    optics.fit(distrib_)
+    labels = optics.labels_
+    uniq_labels = torch.unique(torch.tensor(labels))
+
+    client_cluster_index = dict()
+    for i, lab in enumerate(labels):
+        client_cluster_index[i] = int(lab + 1)
+
+    return client_cluster_index, distrib_
+
+def partition_data(dataset, _iid: int, non_iid_diff : int, num_clients: int, alpha: float, beta: float, dataset_name='cifar10'):
+    assert _iid + non_iid_diff <= num_clients, 'Check num_iid, non_iid_diff and num_clients.'
+
+    classes_ = dataset.classes
+    num_classes = len(classes_)
+
+    client_size = len(dataset) // num_clients
+    label_size = len(dataset) // num_classes
+
+    indices_class = [[] for _ in range(num_classes)]
+
+    for i, lab in enumerate(dataset.targets):
+        indices_class[lab].append(i)
 
     if dataset_name == 'sentimen140':
-        if model_name == 'lstm':
-            return LSTM()
-        else:
-            raise ValueError(f"Invalid model name '{model_name}' for dataset '{dataset_name}'")
+        non_iid_labels = list(range(2))
+        id_non_iid_clients_size = client_size
+
+    elif dataset_name == 'cifar10' or dataset_name == 'fmnist':
+        non_iid_labels = random.sample(range(num_classes), 2)
+        id_non_iid_clients_size = client_size
 
     elif dataset_name == 'emnist':
-        if model_name == 'cnn':
-            return CNN(num_layer=model_config['num_layer'], 
-                       in_shape=model_config['in_shape'],
-                       im_size=model_config['im_size'],
-                       hidden=model_config['hidden'], 
-                       out_shape=model_config['out_shape'])
-        else:
-            raise ValueError(f"Invalid model name '{model_name}' for dataset '{dataset_name}'")
-
-    elif dataset_name == 'fmnist':
-        if model_name == 'cnn':
-            return CNN(num_layer=model_config['num_layer'], 
-                       in_shape=model_config['in_shape'],
-                       im_size=model_config['im_size'],
-                       hidden=model_config['hidden'], 
-                       out_shape=model_config['out_shape'])
-        else:
-            raise ValueError(f"Invalid model name '{model_name}' for dataset '{dataset_name}'")
-
-    elif dataset_name == 'cifar10':
-        if model_name == 'cnn':
-            return CNN(num_layer=model_config['num_layer'], 
-                       im_size=model_config['in_shape'],
-                       hidden=model_config['hidden'], 
-                       out_shape=model_config['out_shape'])
-        elif model_name == 'resnet50':
-            return ResNet(ResidualBlock, [3, 4, 6, 3], num_classes=model_config['out_shape'])
-        elif model_name == 'resnet101':
-            return ResNet(ResidualBlock, [3, 4, 23, 3], num_classes=model_config['out_shape'])
-        elif model_name == 'vgg16':
-            return VGG16(in_channels=model_config['in_shape'], num_classes=model_config['out_shape'])
-        else:
-            raise ValueError(f"Invalid model name '{model_name}' for dataset '{dataset_name}'")
+        non_iid_labels = list(range(10))
+        id_non_iid_clients_size = client_size
 
     elif dataset_name == 'cifar100':
-        if model_name == 'cnn':
-            return CNN(num_layer=model_config['num_layer'], 
-                       im_size=model_config['in_shape'],
-                       hidden=model_config['hidden'], 
-                       out_shape=model_config['out_shape'])
-        elif model_name == 'resnet50':
-            return ResNet(ResidualBlock, [3, 4, 6, 3], num_classes=model_config['out_shape'])
-        elif model_name == 'resnet101':
-            return ResNet(ResidualBlock, [3, 4, 23, 3], num_classes=model_config['out_shape'])
-        elif model_name == 'vgg16':
-            return VGG16(in_channels=model_config['in_shape'], num_classes=model_config['out_shape'])
-        else:
-            raise ValueError(f"Invalid model name '{model_name}' for dataset '{dataset_name}'")
+        non_iid_labels = random.sample(range(num_classes), 15)
+        id_non_iid_clients_size = client_size
 
-    else:
-        raise ValueError(f"Invalid dataset name '{dataset_name}'")
+    non_iid_data = []
+    labels = list(range(num_classes))
 
+    for label in non_iid_labels:
+        non_iid_data += indices_class[label]
 
-def train(net, 
-          trainloader, 
-          criterion, 
-          optimizer, 
-          device, 
-          proximal_mu: float = None):
+    ids = []
+    label_dist = []
+
+    print('Processing non-iid and iid---')
+    for i in tqdm(range(non_iid_diff + _iid)):
+        concentration = torch.ones(len(labels)) * (alpha if i < _iid else beta)
+        dist = Dirichlet(concentration).sample()
+
+        client_indices = []
+        for _ in range(client_size):
+            if not labels:
+                break
+
+            label = random.choices(labels, dist)[0]
+            if indices_class[label]:
+                id_sample = random.choice(indices_class[label])
+                client_indices.append(id_sample)
+                indices_class[label].remove(id_sample)
+
+                if not indices_class[label]:
+                    dist = renormalize(dist, labels, label)
+                    labels.remove(label)
+
+        ids.append(client_indices)
+        counter = Counter(list(map(lambda x: dataset[x][1], ids[i])))
+        label_dist.append({classes_[j]: counter.get(j, 0) for j in range(num_classes)})
     
-    net.train()
-    running_loss, running_corrects = 0.0, 0
+    print('Processing identical distributed non-iid')
+    for i in tqdm(range(non_iid_diff + _iid, num_clients)):
 
-    global_params = copy.deepcopy(net).parameters()
+        temp_data = non_iid_data.copy()
+        id_sample = random.sample(temp_data, id_non_iid_clients_size)
+        ids.append(id_sample)
 
-    for images, labels in trainloader:
-        images, labels = images.to(device), labels.to(device)
-        optimizer.zero_grad()
-        outputs = net(images)
+        counter = Counter(list(map(lambda x: dataset[x][1], ids[i])))
+        label_dist.append({classes_[j]: counter.get(j, 0) for j in range(num_classes)})
 
-        loss = criterion(outputs, labels)
-
-        if proximal_mu is not None:
-            proximal_term = sum((local_weights - global_weights).norm(2) 
-                                for local_weights, global_weights in zip(net.parameters(), global_params))
-            loss += (proximal_mu / 2) * proximal_term
-
-        loss.backward()
-        optimizer.step()
-
-        predicted = torch.argmax(outputs, dim=1)
-        running_corrects += torch.sum(predicted == labels).item()
-        running_loss += loss.item() * images.shape[0]
-
-        del images, labels, outputs, loss, predicted
-
-    running_loss /= len(trainloader.dataset)
-    accuracy = running_corrects / len(trainloader.dataset)
-
-    del global_params
-    torch.cuda.empty_cache()
-    gc.collect()
-
-    return running_loss, accuracy
+    return ids, label_dist
 
 
-def test(net, testloader, device):
-    net.eval()
-    criterion = nn.CrossEntropyLoss()
-    corrects, total_loss = 0, 0.0
-
-    with torch.no_grad():
-        for images, labels in testloader:
-            images, labels = images.to(device), labels.to(device)
-            outputs = net(images)
-            loss = criterion(outputs, labels)
-
-            predicted = torch.argmax(outputs, dim=1)
-            corrects += torch.sum(predicted == labels).item()
-            total_loss += loss.item() * images.shape[0]
-
-            del images, labels, outputs, predicted
-            torch.cuda.empty_cache()
-
-    total_loss /= len(testloader.dataset)
-    accuracy = corrects / len(testloader.dataset)
-
-    torch.cuda.empty_cache()
-    gc.collect()
-
-    return total_loss, accuracy
-
-
-
-def get_parameters(net) -> List[np.ndarray]:
-    return [val.cpu().numpy() for _, val in net.state_dict().items()]
-
-
-def set_parameters(net, parameters: List[np.ndarray]):
-    params_dict = zip(net.state_dict().keys(), parameters)
-    state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
-    net.load_state_dict(state_dict)
-
-
-def compute_entropy(counts: Dict):
-    entropy = 0.0
-    counts = list(counts.values())
-    counts = [0 if value is None else value for value in counts]
-    for value in counts:
-        entropy += -value/sum(counts) * math.log(value/sum(counts), len(counts)) if value != 0 else 0
-    return entropy
