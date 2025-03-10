a78a4bb45d0b409a47a7c5499a47532bdf40f059
Quang Duy <121656882+GanDaffe@users.noreply.github.com>
2025-03-10 11:50:19 +0700

    Update client.py

--- algorithm/fedadpimp/client.py
+++ algorithm/fedadpimp/client.py
@@ -11,5 +11,5 @@ class ClusterFedClient(BaseClient):
     def fit(self, parameters, config):
         set_parameters(self.net, parameters)
         optimizer = torch.optim.SGD(params=self.net.parameters(), lr=config['learning_rate'])
-        loss, accuracy = train(self.net, self.trainloader, self.criterion, optimizer)
+        loss, accuracy = train(self.net, self.trainloader, self.criterion, optimizer, device=self.device)
         return self.get_parameters(self.net), len(self.trainloader.sampler), {"loss": loss, "accuracy": accuracy, "id": self.cid, "cluster_id": self.cluster_id}
