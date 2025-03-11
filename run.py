039432105b444e02d3a19c0c70b3b1f94e3e0f91
Quang Duy <121656882+GanDaffe@users.noreply.github.com>
2025-03-12 00:40:34 +0700

    Update run.py

--- run.py
+++ run.py
@@ -183,6 +183,7 @@ def run_simulation(
                 algo_name           = algo, 
                 net                 = net,
                 testloader          = testloader, 
+                device              = device,
                 num_rounds          = exp_config['num_round'], 
                 num_clients         = exp_config['num_clients'], 
                 iids                = exp_config['iids'], 
